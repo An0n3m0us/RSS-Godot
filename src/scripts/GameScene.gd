5d4627e830568ad88f2dd35a6991036bef17f7ae
An0n3m0us <an0n3m0us@hotmail.com>
2020-04-23 00:48:52 +0100

    Add template selection

--- src/scripts/GameScene.gd
+++ src/scripts/GameScene.gd
@@ -1,16 +1,40 @@
-extends Control
+extends Node2D
 
+var dragging = false  # Are we currently dragging?
+var selected = []  # Array of selected units.
+var drag_start = Vector2.ZERO  # Location where drag began.
+var select_rect = RectangleShape2D.new()  # Collision shape for drag box.
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Big thanks to KidsCanCode for drag-select code
 
+func _input(event):
+	print(event)
+	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT:
+		if event.pressed:
+			if selected.size() == 0:
+				dragging = true
+				drag_start = event.position
+			else:
+				for item in selected:
+					item.collider.target = event.position
+					$Knight/ColorRect.visible = false
+				selected = []
+		elif dragging:
+			dragging = false
+			update()
+			var drag_end = event.position
+			select_rect.extents = (drag_end - drag_start) / 2
+			var space = get_world_2d().direct_space_state
+			var query = Physics2DShapeQueryParameters.new()
+			query.set_shape(select_rect)
+			query.transform = Transform2D(0, (drag_end + drag_start) / 2)
+			selected = space.intersect_shape(query)
+			for item in selected:
+				$Knight/ColorRect.visible = true
+	if event is InputEventMouseMotion and dragging:
+		update()
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _draw():
+	if dragging:
+		draw_rect(Rect2(drag_start, get_global_mouse_position() - drag_start),
+			Color(0, .5, .5, 0.5), true)
