7f2d8f080066d8f267dbae5d9fb6e48c83ca2a85
An0n3m0us <an0n3m0us@hotmail.com>
2020-05-04 18:47:52 +0200

    Fix unitpanel not moving with viewport

--- src/actors/Esquire.gd
+++ src/actors/Esquire.gd
@@ -18,29 +18,31 @@ func _input(event):
 
 func _physics_process(delta):
 	var movement = position.direction_to(target) * speed
+	
+	# Check if distance is greater than 5
 	if position.distance_to(target) > 5:
 		velocity = move_and_collide(movement * delta)
 
 	if velocity:
 		target = position
-		
-		#if velocity.collider.type != type:
-		#	health -= 0.05
 
+	# Check in which direction the character is moving
 	if target[0] < position[0]:
 		self.get_node("Pivot").scale = Vector2(-1, 1)
 	elif target[0] > position[0]:
 		self.get_node("Pivot").scale = Vector2(1, 1)
 
+	# Check if colliding with object
 	if healthdeplete == true:
 		health -= 0.05
 
 	update()
 
 func _draw():
+	# Health bar
 	#if selected == true:
-		draw_rect(Rect2(self.get_node("Pivot").position[0]-21, self.get_node("Pivot").position[1]-110, 50, 5), Color8(100, 0, 0))
-		draw_rect(Rect2(self.get_node("Pivot").position[0]-21, self.get_node("Pivot").position[1]-110, health*5, 5), Color8(255, 0 , 0))
+	draw_rect(Rect2(self.get_node("Pivot").position[0]-21, self.get_node("Pivot").position[1]-110, 50, 5), Color8(100, 0, 0))
+	draw_rect(Rect2(self.get_node("Pivot").position[0]-21, self.get_node("Pivot").position[1]-110, health*5, 5), Color8(255, 0 , 0))
 
 func _on_Area2D_body_entered(body):
 	if body.type != type:
--- src/scripts/GameScene.gd
+++ src/scripts/GameScene.gd
@@ -26,7 +26,7 @@ func _input(event):
 			var player = scene.instance()
 			var rng = RandomNumberGenerator.new()
 			rng.randomize()
-			player.name = "Esquire-enemy-"+str(rng.randf_range(-10.0, 10.0))
+			player.name = "Esquire_enemy-"+str(rng.randf_range(-10.0, 10.0))
 			player.position = get_global_mouse_position()
 			player.target = player.position
 			get_node("Units").add_child(player)
@@ -62,33 +62,33 @@ func _draw():
 		draw_rect(Rect2(drag_start, get_global_mouse_position() - drag_start),
 			Color(0, .5, .5, 0.5), true)
 			
-	for node in get_node("UnitPanel").get_children():
-		get_node("UnitPanel").remove_child(node)
+	for node in get_node("Camera2D/UnitPanel").get_children():
+		get_node("Camera2D/UnitPanel").remove_child(node)
 
 func _process(_delta):
 	
 	# Unit panel
 	var list = get_node("Units").get_children()
 	var panelnames = []
-	for nodes in get_node("UnitPanel").get_children():
+	for nodes in get_node("Camera2D/UnitPanel").get_children():
 		panelnames.append(nodes.name)
 	for unit in range(0, list.size()):
-		var i = get_node("UnitPanel").get_children().size()
+		var i = get_node("Camera2D/UnitPanel").get_children().size()
 		
 		# Unit icon when selected
 		if list[unit].selected == true:
 			if not list[unit].name in panelnames:
 				var uniticonscene = load("res://src/unitpanel/UnitIcon.tscn")
 				var uniticon = uniticonscene.instance()
-				if list[unit].name.split("-")[0]:
+				if list[unit].name.split("-")[0] == "Esquire":
 					uniticon.get_child(0).texture = load("res://assets/images/unitpanel/normalicon.png")
 				else:
 					uniticon.get_child(0).texture = load("res://assets/images/unitpanel/enemyicon.png")
 				uniticon.set_name(list[unit].name)
 				uniticon.position = Vector2(360+(i*50)+(i*15), 655)
-				uniticon.get_child(1).set_text(list[unit].name.split("-")[0])
+				uniticon.get_child(1).set_text(list[unit].name.split("-")[0].split("_")[0])
 				uniticon.get_child(3).set_size(Vector2(list[unit].health*5, 5))
-				get_node("UnitPanel").add_child(uniticon)
+				get_node("Camera2D/UnitPanel").add_child(uniticon)
 		# Check health
 		if list[unit].health <= 0:
 			get_node("Units").remove_child(list[unit])
