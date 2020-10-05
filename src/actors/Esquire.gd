9e1d2c8a57653f567d35b739b46c2419e7bc4015
An0n3m0us <an0n3m0us@hotmail.com>
2020-10-05 22:49:29 +0100

    Add spawning units

---
+++ src/actors/Esquire.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+export (String) var type
+export (float) var healthMultiplier
+export (int) var speed
+
+var selected = false
+
+var health = 10
+var healthdeplete = false
+
+func _physics_process(delta):
+
+	# Check if colliding with object
+	if healthdeplete:
+		health -= healthMultiplier
+
+	update()
+
+func _draw():
+	# Health bar
+	#if selected == true:	
+	draw_rect(Rect2(self.get_node("Pivot").position[0]-24, self.get_node("Pivot").position[1]-60, 50, 5), Color8(100, 0, 0))
+	draw_rect(Rect2(self.get_node("Pivot").position[0]-24, self.get_node("Pivot").position[1]-60, health*5, 5), Color8(255, 0 , 0))
+
+func _on_Area2D_body_entered(body):
+	if body.type != type:
+		healthdeplete = true
+
+func _on_Area2D_body_exited(body):
+	if body.type != type:
+		healthdeplete = false
--- src/scenes/GameScene.gd
+++ src/scenes/GameScene.gd
@@ -6,12 +6,9 @@ var size = self.rect_size*2
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed:
 		if (event.position - self.rect_position-size).length() < click_radius:
-			print(self.name)
-			print(self.rect_position)
-			var s = ColorRect.new()
-			s.set_name("node")
-			s.set_size(Vector2(50, 50))
-			s.set_position(Vector2(self.rect_position[0]+25, self.rect_position[1]+25))
-			get_parent().add_child(s) # Add it as a child of this node.
-			var x = get_parent().get_children()[-1]
-			x.color = Color(randf(), randf(), randf())
+			if (get_parent().has_node(self.name + "_unit") == false):
+				var scene = load("res://src/actors/Esquire.tscn")
+				var player = scene.instance()
+				player.name = self.name + "_unit"
+				player.position = Vector2(self.rect_position[0]+50, self.rect_position[1])
+				get_parent().add_child(player)
